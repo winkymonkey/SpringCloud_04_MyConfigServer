b867798253de46cef54c53c33ba70904bacae19d
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-22 11:49:13 +0530

    refactor

--- src/main/java/org/example/demo/MyConfigServerApplication.java
+++ src/main/java/org/example/demo/MyConfigServerApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package org.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
 @EnableConfigServer
 @SpringBootApplication
 public class MyConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyConfigServerApplication.class, args);
 	}
